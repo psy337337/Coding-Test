bca54d7216d5505fc886f08db00f95023106a0a9
psy337337 <psy337337@naver.com>
2023-03-21 01:28:44 +0900

    [Silver II] Title: 베르트랑 공준, Time: 216 ms, Memory: 17036 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,42 +1,38 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		ArrayList<Integer> arr = new ArrayList<Integer>();
-		int[] last;
-		int[] base;
-		int x = 0;
-		int max = 0;
-		int count;
+	public static void main(String[] args) throws IOException{
+		int max, cnt = 0;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		ArrayList<Integer> input = new ArrayList();
+		boolean arr[];
+		
 		while(true) {
-			arr.add(sc.nextInt());
-			if(arr.get(x)>123456)
-				arr.remove(x);
-			if(arr.get(x)!=null && max < arr.get(x))
-				max = arr.get(x);
-			if(arr.get(x)==0)
+			input.add(Integer.parseInt(br.readLine()));
+			if(input.get(input.size()-1) == 0)
 				break;
-			x++;
 		}
-		base = new int[max*2+1];
-		last = new int[arr.size()];
-		for(int i = 2; i < base.length; i++) {
-			base[i] = i;
-		}
-		for(int i = 2; i <= Math.sqrt(max*2);i++) {
-			if(base[i]==0)
+		max = Collections.max(input);
+		arr = new boolean[2*max+1];
+		arr[1] = true;
+		for(int i = 2; i < Math.sqrt(2*max)+1; i++) {
+			if(arr[i]) 
 				continue;
-			for(int j = i+i; j <= max*2; j+=i)
-				base[j] = 0;
+			for(int j = i*i; j < 2*max+1; j=j+i)
+				arr[j] = true;
 		}
-		for(int i = 0; i < arr.size(); i++) {
-			for(int j = arr.get(i)+1; j <= arr.get(i)*2;j++) {
-				if(base[j] != 0)
-					last[i]++;
+		
+		for(int i = 0; i < input.size()-1;i++) {
+			cnt = 0;
+			for(int j = input.get(i)+1; j < 2*input.get(i)+1;j++) {
+				if(!arr[j])
+					cnt++;
 			}
+			System.out.println(cnt);
 		}
-		for(int i = 0; i < last.length-1; i++)
-			System.out.println(last[i]);
 	}
 }
